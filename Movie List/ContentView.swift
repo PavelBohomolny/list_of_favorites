a64f0a817c670f68403505e0f5fa03731256c50f
NexusUA <pbogomolny93@gmal.com>
2022-09-02 11:47:21 +0200

    Initial Commit

---
+++ Movie List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie List
+//
+//  Created by Pavel Bohomolnyi on 02/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie List
+//
+//  Created by Pavel Bohomolnyi on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
